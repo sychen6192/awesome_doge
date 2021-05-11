beb9cb384d8b885e64c2b94ed18394b80cefea5d
Jack SY Chen <jack_sy_chen@trendmicro.com>
2021-05-11 23:03:50 +0800

    Updates

---
+++ precache-manifest.5f7dbeff974bd75174b3c569f0a83fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b380518bb50438bf36e0e04c2ccd5b49",
+    "url": "/awesome_doge/index.html"
+  },
+  {
+    "revision": "52c06f64d286b8bc9d52",
+    "url": "/awesome_doge/static/css/main.b0c4f4fb.chunk.css"
+  },
+  {
+    "revision": "063dab20ad9702daa43e",
+    "url": "/awesome_doge/static/js/2.ade57f91.chunk.js"
+  },
+  {
+    "revision": "46bc69453da2fc264c76d97a813e2bcf",
+    "url": "/awesome_doge/static/js/2.ade57f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52c06f64d286b8bc9d52",
+    "url": "/awesome_doge/static/js/main.a44e9195.chunk.js"
+  },
+  {
+    "revision": "aede80fca203363f7b51",
+    "url": "/awesome_doge/static/js/runtime-main.555581d9.js"
+  }
+]);
